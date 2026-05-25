f17837d17579312fc0d553eb3ad6c7013f3fb47f
Augustine <augustine@Augustines-MacBook-Pro.local>
2026-05-25 12:24:34 +0530

    Other changes.

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,10 @@
 // @ts-check
 import { defineConfig } from '@playwright/test';
 
-import dotenv from 'dotenv';
+import dotenvExtended from 'dotenv-extended';
 import os from 'os';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, 'tests/config', 'goal.test.env'), debug: false });
+dotenvExtended.load({ path: 'tests/config/goal.test.env', silent: true });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
